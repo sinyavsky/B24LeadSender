bd39db6ff1b6a9aa91bf2111f1ecc12caaad856f
Alexander Sinyavsky <inbox@sinyavsky.com>
2022-08-03 00:32:20 +0300

    обновить демо скрипт

--- demo.php
+++ demo.php
@@ -2,39 +2,79 @@
 
 require("B24LeadSender.php");
 
-// При создании объекта передаем все необходимые для коннекта к Б24 данные:
-// 1. УРЛ Битрикс24 (без слэша в конце)
-// 2. Строку-идентификатор веб-хука
-// 3. Айди пользователя, ответственного за лид
-// 4. Айди пользователя с правами администратора
-// как создать веб-хук - я расписал в этой статье: https://alfavitka.ru/sozdaem-lid-bitrix24-cherez-api/
+$restApiUrl = "https://demo.bitrix24.ru/rest/1/etodemovveditesvoidannye/crm.lead.add.json"; // урл из настроек Б24
+$userId = 1; // ID пользователя, который будет ответственным за лид
 
-// В этом примере адрес Б24 и строка веб-хука конечно-же фейковые
-$leadSender = new \B24LeadSender\LeadSender("https://demo.bitrix24.ru", "hehehe1demo2hook3", 1, 1);
+$leadSender = new \Sinyavsky\B24LeadSender($restApiUrl, $userId);
 
-// Заполняем основные данные: имя контакта, телефон(ы), емейл(ы), заголовок лида и комментарий к нему
+// теперь заполняем данные по лиду
 
-// мечтать не вредно
+// имя :D
 $leadSender->SetName("Меган Фокс");
 
-// это пример, её настоящий номер я вам не дам, конечно же
-$leadSender->AddPhone("+71234567890");
-// аналогично можем добавить столько номеров, сколько захотим
-//$leadSender->AddPhone("+71234567891");
 
-// то же самое с емейлом
-$leadSender->AddEmail("meganfox@alfavitka.ru");
-//$leadSender->AddEmail("meganfox2@alfavitka.ru");
+// номер телефона и его тип, на момент написания в Б24 есть такие типы:
+// WORK, MOBILE, FAX, HOME, PAGER, MAILING, OTHER
+// но можем указать и свой тип, Б24 все-равно его примет
 
-// если не устанавливать тайтл явно, то в заголовке лида будет указано то, что мы передали в SetName
+$leadSender->AddPhone("+7 111 111-11-11", "WORK"); // рабочий номер
+
+// можем добавить столько номеров, сколько захотим
+$leadSender->AddPhone("+7 222 222-22-22", "MOBILE"); // мобильный номер
+$leadSender->AddPhone("+7 333 333-33-33"); // можем тип не указывать, по-умолчанию он будет MOBILE
+$leadSender->AddPhone("+7 444 444-44-44", "Еще какой-то номер"); // в Б24 телефон так и подпишется: "Еще какой-то номер"
+
+
+// с емейлом всё аналогично, на момент написания в Б24 есть такие типы:
+// WORK, HOME, MAILING, OTHER
+// но можем указать и свой тип
+
+$leadSender->AddEmail("megan111@sinyavsky.com", "WORK"); // рабочий емейл
+
+// как и с телефоном: можем добавить сколько угодно адресов
+$leadSender->AddEmail("megan222@sinyavsky.com", "HOME"); // домашний емейл
+$leadSender->AddEmail("megan333@sinyavsky.com"); // можем тип не указывать, по-умолчанию он будет WORK
+$leadSender->AddEmail("megan444@sinyavsky.com", "Какой-то еще"); // в Б24 емейл так и подпишется: "Какой-то еще"
+
+
+// устанавливаем заголовок лида
 $leadSender->SetTitle("Заказ обратного звонка от: Меган Фокс");
 
-// комментарий к лиду
-$leadSender->SetComments("Срочно нужен Chevrolet Camaro 1977 года! Плачу наличными!!!");
 
-// Когда все данные указали - пытаемся отправить лид
+// заполняем поле "Комментарий"
+$leadSender->SetComments("Есть очень важное дело, срочно перезвоните!");
+
+
+// устанавливаем значения пользовательских полей
+$leadSender->SetUserField("UF_CRM_4104475401715", "Дополнительное текстовое поле"); // текстовое поле
+$leadSender->SetUserField("UF_CRM_1101425437375", true); // поле типа да/нет
+
+
+// в Б24 есть отдельные поля для UTM-меток, можем установить и их
+$leadSender->SetUtmSource("primer_utm_source");
+$leadSender->SetUtmMedium("primer_utm_medium");
+$leadSender->SetUtmCampaign("primer_utm_campaign");
+$leadSender->SetUtmContent("primer_utm_content");
+$leadSender->SetUtmTerm("primer_utm_term");
+
+
+// если надо заполнить еще какое-то поле, для которого нет отдельного метода
+// то используем метод SetOther, список всех полей можно посмотреть в документации:
+// https://dev.1c-bitrix.ru/rest_help/crm/leads/crm_lead_fields.php
+$leadSender->SetOther("ADDRESS_COUNTRY", "США"); // например, можем указать страну
+
+
+// когда все данные заполнили - отправляем лид
 if ($leadSender->Send()) {
     echo "<p>Лид успешно отправлен.</p>";
-} else {
-    echo "<p>При отправке лида возникла ошибка: {$leadSender->GetError()}";
+} else { // если лид не отправлен - в GetError() будет текст ошибки
+    echo "<p>При отправке лида возникла ошибка: {$leadSender->GetError()}</p>";
+
+    // сразу можем посмотреть, какие данные мы пытались отправить
+    // чтобы быстрее найти причину ошибки
+    /*
+        echo "<pre>";
+        print_r($leadSender->GetQueryData());
+        echo "</pre>";
+    */
 }
